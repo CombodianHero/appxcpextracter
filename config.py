cba939a47ca0ac886f5f6f03b00e9d00c5a0a71f
Hero Sundra <jaibajarangclasses@gmail.com>
2025-06-08 23:08:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "21705536"
 api_hash = "c5bb241f6e3ecf33fe68a444e288de2d"
 bot_token = "7648371277:AAHmRSZXqXF9ceajpsJVsok2q0eFceceBJE"
-auth_users = [7809571505, 7793444237]
+#auth_users = [7809571505, 7793444237]
