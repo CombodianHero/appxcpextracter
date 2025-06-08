369b8383dc5800651b8b434c471da14321ed6101
Hero Sundra <jaibajarangclasses@gmail.com>
2025-06-08 22:59:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def start(bot, message):
 
   await message.reply_photo(
     photo=random_image_url,
-    caption="**Developer - @pwextractowner\nPLEASE👇PRESS👇HERE**",
+    caption="**Developer - @erpurchasebot\nPLEASE👇PRESS👇HERE**",
     quote=True,
     reply_markup=reply_markup
   )
